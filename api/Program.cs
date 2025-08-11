781b51d472dd3807bed42cc71f804d1ff1f7e27f
dualUbuntuLevi <leviqueiroz647@gmail.com>
2025-08-11 13:28:28 -0300

    Testing route in Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -3,4 +3,22 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (LoginDTO loginDto) => 
+{
+    if (loginDto.Email == "user@example.com" && loginDto.Password == "password")
+    {
+        return Results.Ok("Login successful");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
 app.Run();
+
+public class LoginDTO
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
